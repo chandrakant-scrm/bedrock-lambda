01add91385e96af96a1b9209da48fac6764ae4c5
chandrakant-scrm <chandrakant.shinde@simplecrm.com>
2024-06-16 19:52:13 +0530

    black applied on app.py

--- app.py
+++ app.py
@@ -4,36 +4,38 @@ import botocore.config
 from datetime import datetime
 
 
-def blog_generate_using_bedrock(blogtopic:str) -> str:
-    prompt=f"""<s>[INST]Human: Write a 200 words blog on the topic {blogtopic}
+def blog_generate_using_bedrock(blogtopic: str) -> str:
+    prompt = f"""<s>[INST]Human: Write a 200 words blog on the topic {blogtopic}
     Assistant:[/INST]
     """
-    body={
-        "prompt":prompt,
-        "max_gen_len":512,
-        "temperature":0.5,
-        "top_p":0.9
-    }
-    
+    body = {"prompt": prompt, "max_gen_len": 512, "temperature": 0.5, "top_p": 0.9}
+
     try:
-        bedrock=boto3.client("bedrock-runtime", region_name="us-east-1",
-                             config=botocore.config.Config(read_timeout=300, retries={'max_attempts':3}))
-        
-        response=bedrock.invoke_model(body=json.dumps(body),modelId="meta.llama3-8b-instruct-v1:0")
-        
-        response_content=response['Body'].read()
-        response_data=json.loads(response_content)
+        bedrock = boto3.client(
+            "bedrock-runtime",
+            region_name="us-east-1",
+            config=botocore.config.Config(
+                read_timeout=300, retries={"max_attempts": 3}
+            ),
+        )
+
+        response = bedrock.invoke_model(
+            body=json.dumps(body), modelId="meta.llama3-8b-instruct-v1:0"
+        )
+
+        response_content = response["Body"].read()
+        response_data = json.loads(response_content)
         print(response_data)
-        
-        blog_details=response_data['generation']
+
+        blog_details = response_data["generation"]
         return blog_details
     except Exception as e:
         print(e)
-    
+
 
 def save_blog_details_s3(s3_key, s3_bucket, generate_blog):
-    s3=boto3.client("s3")
-    
+    s3 = boto3.client("s3")
+
     try:
         s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=generate_blog)
         print("saved to s3")
@@ -42,19 +44,16 @@ def save_blog_details_s3(s3_key, s3_bucket, generate_blog):
 
 
 def lambda_handler(event, context):
-    event=json.loads(event['body'])
-    blogtopic=event['blog_topic']
-    
-    generate_blog=blog_generate_using_bedrock(blogtopic=blogtopic)
+    event = json.loads(event["body"])
+    blogtopic = event["blog_topic"]
+
+    generate_blog = blog_generate_using_bedrock(blogtopic=blogtopic)
     if generate_blog:
-        current_time=datetime.now().strftime("%H%M%S")
-        s3_key=f"blog-output/{current_time}.txt"
-        s3_bucket="aws-bedrockcourse1"
+        current_time = datetime.now().strftime("%H%M%S")
+        s3_key = f"blog-output/{current_time}.txt"
+        s3_bucket = "aws-bedrockcourse1"
         save_blog_details_s3(s3_key, s3_bucket, generate_blog)
     else:
         print("No blog was generated")
-        
-    return {
-        'statusCode':200,
-        'body':json.dumps('Blog generated!')
-    }
+
+    return {"statusCode": 200, "body": json.dumps("Blog generated!")}
